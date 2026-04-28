587e3285b660379ee363d5187646408e825d6c66
nadja-acimovic <nadjaacimovic13@gmail.com>
2026-04-28 22:44:57 +0200

    add attendance statistics for employees

--- src/app/pages/zaposleni/statistika/statistika.page.ts
+++ src/app/pages/zaposleni/statistika/statistika.page.ts
@@ -1,20 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardContent, IonIcon } from '@ionic/angular/standalone';
+import { Firestore, collection, getDocs, query, orderBy } from '@angular/fire/firestore';
+import { addIcons } from 'ionicons';
+import { personOutline, calendarOutline, timeOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-statistika',
   templateUrl: './statistika.page.html',
   styleUrls: ['./statistika.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardContent, IonIcon, CommonModule]
 })
 export class StatistikaPage implements OnInit {
 
-  constructor() { }
+  prisustva: any[] = [];
+  ukupno: number = 0;
+  danas: number = 0;
 
-  ngOnInit() {
+  constructor(private firestore: Firestore) {
+    addIcons({ personOutline, calendarOutline, timeOutline });
   }
 
+  async ngOnInit() {
+    await this.ucitajStatistiku();
+  }
+
+  async ucitajStatistiku() {
+    const prisustvoRef = collection(this.firestore, 'prisustvo');
+    const q = query(prisustvoRef, orderBy('datum', 'desc'));
+    const querySnap = await getDocs(q);
+
+    this.prisustva = querySnap.docs.map(doc => {
+      const data = doc.data();
+      return {
+        ...data,
+        datumFormatiran: new Date(data['datum']).toLocaleDateString('sr-RS', {
+          day: '2-digit',
+          month: '2-digit',
+          year: 'numeric',
+          hour: '2-digit',
+          minute: '2-digit'
+        })
+      };
+    });
+
+    this.ukupno = this.prisustva.length;
+
+    // Broj dolazaka danas
+    const danas = new Date();
+    const pocetak = new Date(danas.getFullYear(), danas.getMonth(), danas.getDate()).toISOString();
+    const kraj = new Date(danas.getFullYear(), danas.getMonth(), danas.getDate() + 1).toISOString();
+    this.danas = this.prisustva.filter(p => p.datum >= pocetak && p.datum < kraj).length;
+  }
 }
